4e41118326e977f78e0842db27ee204e2f5c7e3f
mansurbek <matniyozovw.o10@gmail.com>
2026-01-29 20:15:02 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* =========================== typing animation =========================== */
+var typed = new Typed(".typing", {
+    strings: [ "Front-end developer",  "Web Developer", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
